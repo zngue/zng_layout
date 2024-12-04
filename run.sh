3021ef3037825e461866096e44c5a9726fb83c84
zngue <zngfue@163.com>
2024-12-04 10:45:17 +0800

    更新模板文件的默认数据

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 docker-compose down
 sleep 3
-# 删除未使用的镜像
-docker image prune -f
+docker image prune -f # 删除未使用的镜像
 echo "清理完成"
 git pull
 sleep 3
@@ -11,8 +10,6 @@ sleep 3
 docker-compose up -d
 echo "部署完成"
 docker builder prune -f
-
 echo "缓存镜像清理完成"
-
 docker system prune -f
 echo "清理完成"
