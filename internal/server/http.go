04cf374bc26ecfe11b376116e1f977673aa007da
zngue <zngfue@163.com>
2025-02-19 09:49:23 +0800

    更新模板文件的默认数据

--- internal/server/http.go
+++ internal/server/http.go
@@ -3,13 +3,10 @@ package server
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
 	"github.com/zngue/zng_app/db/api"
 	"github.com/zngue/zng_app/pkg/types"
 	"github.com/zngue/zng_layout/internal/conf"
 	"net/http"
-	"reflect"
 )
 
 func NewHttpService(c *conf.Bootstrap, handler *gin.Engine, routes []types.Register) *http.Server {
@@ -22,12 +19,6 @@ func NewHttpService(c *conf.Bootstrap, handler *gin.Engine, routes []types.Regis
 	}
 }
 func NewHttpEngine() *gin.Engine {
-	binding.Query.Name()
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterTagNameFunc(func(fld reflect.StructField) string {
-			return fld.Tag.Get("json") // 改为使用 `json` 标签
-		})
-	}
 	engine := gin.New()
 	engine.Use(gin.Recovery(), gin.Logger())
 	engine.GET("/ping", func(ctx *gin.Context) {
